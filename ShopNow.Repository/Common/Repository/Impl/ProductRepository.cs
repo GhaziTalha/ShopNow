e091cbe9b6f7d1e4bcaf313f0f8efa182ebc3a59
Ghazi Talha <ghazitalha6@gmail.com>
2023-09-11 12:33:26 +0530

    Added Test controler part 1

---
+++ ShopNow.Repository/Common/Repository/Impl/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShopNow.Model;
+using ShopNow.Repository.Common.Repository.Interface;
+using ShopNow.Repository.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopNow.Repository.Common.Repository.Impl
+{
+     public class ProductRepository:EFRepositoryBase<ECommerceContext,Product,int>,IProductRepository
+    {
+    }
+}
---
+++ ShopNow.Repository/Common/Repository/Interface/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using ShopNow.Model;
+
+namespace ShopNow.Repository.Common.Repository.Interface
+{
+    public interface ICategoryRepository:IRepository<Category,int>
+    {
+    }
+}
